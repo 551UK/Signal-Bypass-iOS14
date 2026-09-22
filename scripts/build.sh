40793a95a2e068b5f83cddae52e2dab1b2e4b0a6
551 <wdf@hotmail.co.uk>
2026-09-22 16:04:23 +0100

    Build rootful tweak for arm64 and arm64e

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,14 +6,17 @@ command -v dpkg-deb >/dev/null
 SDK="$(xcrun --sdk iphoneos --show-sdk-path)"
 mkdir -p build package/DEBIAN package/Library/MobileSubstrate/DynamicLibraries
 mkdir -p package/usr/libexec
-xcrun --sdk iphoneos clang -arch arm64 -isysroot "$SDK" \
+xcrun --sdk iphoneos clang -arch arm64 -arch arm64e -isysroot "$SDK" \
   -miphoneos-version-min=14.0 -fobjc-arc -fblocks -O2 -Wall -Wextra \
   -Wno-unused-parameter -Werror -dynamiclib Tweak.m \
   -framework Foundation -framework UIKit \
   -install_name /Library/MobileSubstrate/DynamicLibraries/SignalBypass14.dylib \
   -o build/SignalBypass14.dylib
+xcrun lipo -archs build/SignalBypass14.dylib
+ARCHS="$(xcrun lipo -archs build/SignalBypass14.dylib)"
+[[ "$ARCHS" == *arm64* && "$ARCHS" == *arm64e* ]] || { echo "Missing required rootful architectures: $ARCHS"; exit 1; }
 ldid -S build/SignalBypass14.dylib
-xcrun --sdk iphoneos clang -arch arm64 -isysroot "$SDK" \
+xcrun --sdk iphoneos clang -arch arm64 -arch arm64e -isysroot "$SDK" \
   -miphoneos-version-min=14.0 -fobjc-arc -O2 -Wall -Wextra -Werror \
   -Wno-deprecated-declarations BuildDate.m -framework Foundation -o build/signalbypass14-builddate
 ldid -S build/signalbypass14-builddate
@@ -24,6 +27,6 @@ cp build/SignalBypass14.dylib SignalBypass14.plist package/Library/MobileSubstra
 cp control package/DEBIAN/control
 chmod 755 package package/DEBIAN package/Library package/Library/MobileSubstrate package/Library/MobileSubstrate/DynamicLibraries
 chmod 644 package/DEBIAN/control package/Library/MobileSubstrate/DynamicLibraries/*
-dpkg-deb --root-owner-group -Zgzip --build package build/uk.551.signalbypass14_0.9.0_iphoneos-arm.deb
+dpkg-deb --root-owner-group -Zgzip --build package build/uk.551.signalbypass14_1.0.0_iphoneos-arm.deb
 dpkg-deb --info build/*.deb
 dpkg-deb --contents build/*.deb
